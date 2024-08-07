3b3fe06e9c73d6bcea4789a204c7389dd9ae8f81
Pavel-codes <fabelbutov79@gmail.com>
2024-08-08 02:17:55 +0300

    Light and dark mode css - index page

--- Books/Books/Client/Scripts/index.js
+++ Books/Books/Client/Scripts/index.js
@@ -208,10 +208,27 @@ $(document).ready(function () {
         $('#adminBtn').hide();
     }
 
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme == 'dark' && !document.body.classList.contains('dark-mode')) {
+        document.body.classList.toggle('dark-mode');
+    }
+    else if (currentTheme == 'light' && document.body.classList.contains('dark-mode')) {
+        document.body.classList.toggle('dark-mode');
+    }
 
 });
 
+//const currentTheme = localStorage.getItem('theme');
+const toggleButton = document.getElementById('toggle-mode');
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
 
+    let theme = 'light';
+    if (document.body.classList.contains('dark-mode')) {
+        theme = 'dark';
+    }
+    localStorage.setItem('theme', theme);
+});
 
 
         //All the insert data from API to DB functions
